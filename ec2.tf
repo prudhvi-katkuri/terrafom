91bb61dbecba63f2a10c418f88b890054ba7a3d8
prudhvi-katkuri <92577180+prudhvi-katkuri@users.noreply.github.com>
2021-10-17 21:58:21 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -8,7 +8,7 @@ terraform {
     profile = "default"
     bucket = "terraformstatecode"
     key = "vpc/terraform.tfstate"
-    region = var.region
+    region = "us-east-2"
     
   }
 }
@@ -17,10 +17,18 @@ resource "aws_instance" "demoserver" {
     count = 2
     ami = var.ami
     instance_type = var.instance_type
-    subnet_id = "${element(var.subnet,count.index)}
+    subnet_id = "${element(var.subnet,count.index)}"
     key_name = "devops"
 
-    tags {
-        Name = "demoserver${count.index+1}"
+    user_data = <<-EOF
+        #! /bin/bash
+        sudo apt-get update
+        sudo apt-get install -y apache2
+        sudo systemctl start apache2
+        sudo systemctl enable apache2
+     EOF
+
+    tags = {
+        Name = "demoserver.${count.index+1}"
     }
 }
